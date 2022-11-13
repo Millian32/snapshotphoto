b7343f18a24b25cae389763bde1f13f9a1576c62
Max Wiggs <3708407+Millian32@users.noreply.github.com>
2022-11-13 10:35:14 -0700

    add legacy start command to build

---
+++ build/precache-manifest.efc587519cc6f6ee801cac2ef2ae639e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96c56774040e4157d3a6419f91e3ad73",
+    "url": "/SnapShot/index.html"
+  },
+  {
+    "revision": "7d742ad950aa3f08d2d6",
+    "url": "/SnapShot/static/css/main.d03163bd.chunk.css"
+  },
+  {
+    "revision": "d181d5d7af02efcd3dab",
+    "url": "/SnapShot/static/js/2.c619e43e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SnapShot/static/js/2.c619e43e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d742ad950aa3f08d2d6",
+    "url": "/SnapShot/static/js/main.fac2a8ce.chunk.js"
+  },
+  {
+    "revision": "17f44848bce1e5215d0e",
+    "url": "/SnapShot/static/js/runtime-main.136163d9.js"
+  }
+]);
